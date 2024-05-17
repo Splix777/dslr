a3db9440bf3942e293ac9f9233ebf7fe8e9926fb
splix <federicosalalois@gmail.com>
2024-05-17 13:44:10 +0200

    Removed redundant comments

--- srcs/data_visualization/histogram.py
+++ srcs/data_visualization/histogram.py
@@ -70,46 +70,17 @@ def print_homogeneous_course(df: pd.DataFrame) -> None:
 
 
 if __name__ == '__main__':
-    # csv_path = '/home/splix/Desktop/dslr/csv_files/'
-    # csv_file = 'dataset_train.csv'
-    #
-    # try:
-    #     df = pd.read_csv(csv_path + csv_file)
-    # except FileNotFoundError:
-    #     print(f"Error: File '{csv_file}' not found.")
-    #     exit(1)
-    # except Exception as e:
-    #     print(f"An error occurred while reading the CSV file: {e}")
-    #     exit(1)
-    #
-    # plot_course_distributions(df)
-    # print_homogeneous_course(df)
-
-    # Read the CSV file
     csv_path = '/home/splix/Desktop/dslr/csv_files/'
     csv_file = 'dataset_train.csv'
-    df = pd.read_csv(csv_path + csv_file)
 
-    # Select the course for the histograms
-    course = 'Arithmancy'
+    try:
+        df = pd.read_csv(csv_path + csv_file)
+    except FileNotFoundError:
+        print(f"Error: File '{csv_file}' not found.")
+        exit(1)
+    except Exception as e:
+        print(f"An error occurred while reading the CSV file: {e}")
+        exit(1)
 
-    # Define house colors
-    house_colors = {
-        'Ravenclaw': 'blue',
-        'Slytherin': 'green',
-        'Gryffindor': 'red',
-        'Hufflepuff': 'yellow'
-    }
-
-    # Plot histograms for each house
-    plt.figure(figsize=(10, 6))
-    for house, color in house_colors.items():
-        house_data = df.loc[df['Hogwarts House'] == house, course].dropna()
-        plt.hist(house_data, bins=30, alpha=0.5, color=color, label=house)
-
-    plt.title(f'Histogram of {course} scores by Hogwarts House')
-    plt.xlabel('Score')
-    plt.ylabel('Frequency')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+    plot_course_distributions(df)
+    print_homogeneous_course(df)
